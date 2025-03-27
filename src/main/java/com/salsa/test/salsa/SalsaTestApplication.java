1ba0a2551174a08c733ebcb1b148c3c9b88d57bd
Marcos <marcos.rojas@sngular.com>
2025-03-27 19:43:08 +0100

    first commit

---
+++ src/main/java/com/salsa/test/salsa/SalsaTestApplication.java
@@ -0,0 +1,13 @@
+package com.salsa.test.salsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salsa/test/salsa/SalsaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salsa.test.salsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
